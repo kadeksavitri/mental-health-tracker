cc5c3a310e44a704d83d2049448f768d3581ae78
kadeksavitri <kadeksavitri5@gmail.com>
2024-09-25 14:27:39 +0700

    done tutorial 4

--- main/models.py
+++ main/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class MoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)    
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
